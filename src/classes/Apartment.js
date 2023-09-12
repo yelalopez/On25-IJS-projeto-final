bed721b350e5928b19969dedf7701159cd2b6ac3
yelalopez <yelitzalopezduque@gmail.com>
2023-09-12 19:36:56 -0300

    feat: class Building

---
+++ src/classes/Apartment.js
@@ -0,0 +1,37 @@
+const Building = require("./Building");
+const RecycledPlastic = require("./RecycledPlastic");
+
+
+class Apartment extends RecycledPlastic{
+    name;
+    aptNumber;
+    recycledCountPerApt = 0;
+
+    constructor(idBuilding, name, aptNumber){
+        super()
+        const validateID = Building.buildingsList.find((building) => building.id === idBuilding);
+        if(!validateID){
+            throw new Error('ID de predio inválido')
+        }        
+
+        this.idBuilding = validateID;
+        this.name = name;
+        this.aptNumber = aptNumber;
+        validateID.registerApartment(this.aptNumber);
+ 
+    }
+
+    
+    depositPlastic(weight){
+        super.validateWeight(weight);
+        super.recycledPlastic(weight);
+        this.recycledCountPerApt += weight;
+    }
+
+}
+
+module.exports = Apartment;
+
+
+
+
