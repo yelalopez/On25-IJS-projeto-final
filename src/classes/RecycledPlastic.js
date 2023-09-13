1d7899532e4bfc8b65d6aa333eba80a49e67d806
yelalopez <yelitzalopezduque@gmail.com>
2023-09-13 11:33:18 -0300

    fix: recycledPlastic method modified

--- src/classes/RecycledPlastic.js
+++ src/classes/RecycledPlastic.js
@@ -18,7 +18,11 @@ class RecycledPlastic{
         }else if(weight < 15){
             const contribution = Math.round(((weight * RECYCLED_PLASTIC_LUMBER) / PLASTIC_NEEDED_RPL) * 100)
             return `Sua contribuição para a fabricação de uma peça de madeira plástica reciclada é de ${contribution}%`
-        }
+        }else {
+            const extraUND = Math.floor(weight / PLASTIC_NEEDED_RPL)
+            const extraContribution = Math.round((weight - PLASTIC_NEEDED_RPL) * RECYCLED_PLASTIC_LUMBER);
+            return `Sua contribuição gerou ${extraUND} peça(s) de madeira plástica reciclada e ainda aporto ${extraContribution}% para a fabricação de outra`;
+          }
     }
 
 
